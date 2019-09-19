6c55561bfe62c28b60d065874b6ecc833d6f101e
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-09-19 09:35:13 +0300

    Clean-up.

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -2,13 +2,13 @@ import { expect } from 'chai'
 import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import React from 'react'
-import Hello from '../src/public/components/Hello'
+import MultipleRoutes from '../src/public/components/MultipleRoutes'
 
 Enzyme.configure({ adapter: new Adapter() })
 
-describe('<Hello />', () => {
-  it('renders <Hello />', () => {
-    const wrapper = shallow(<Hello name="tests" />)
+describe('<MultipleRoutes />', () => {
+  it('renders', () => {
+    const wrapper = shallow(<MultipleRoutes />)
     const actual = wrapper.find('h1').text()
     const expected = 'Hello, tests!'
 
